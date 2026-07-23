388db09df8d95b559528a41fa96ebd38c95aaa67
Vignesh <vigneshsarathy26@gmail.com>
2026-07-23 14:59:24 +0530

    feat: implement base runner images, terraform/dind/python toolsets, and troubleshooting documentation

--- base/linux/start.sh
+++ base/linux/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /actions-runner
-curl -o actions-runner-linux-x64-2.335.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.335.1/actions-runner-linux-x64-2.335.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.335.1.tar.gz
-./config.sh --url $RUNNER_URL --token $RUNNER_TOKEN --labels ${RUNNER_LABELS}
+curl -o actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
+tar xzf ./actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
+./config.sh --url $RUNNER_URL --token $RUNNER_TOKEN --labels $RUNNER_LABELS
 ./run.sh
