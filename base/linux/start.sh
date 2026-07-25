4b8615578f7101bda1340305ba8b0dd49bd7e2c9
Vignesh <vigneshsarathy26@gmail.com>
2026-07-25 10:25:50 +0530

    feat: add linux base image, golang runner image, and Android SDK build workflow

--- base/linux/start.sh
+++ base/linux/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 cd /actions-runner
-curl -o actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
-tar xzf ./actions-runner-linux-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
+curl -f -o actions-runner-${TARGETARCH}-${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
+tar xzf ./actions-runner-${TARGETARCH}-${RUNNER_VERSION}.tar.gz
 ./config.sh --url $RUNNER_URL --token $RUNNER_TOKEN --labels $RUNNER_LABELS
 ./run.sh
